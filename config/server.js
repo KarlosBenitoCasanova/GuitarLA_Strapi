dbe58ca0c1ae797a80f7dd5a159af74251fe7280
KarlosBenitoCasanova <104064788+KarlosBenitoCasanova@users.noreply.github.com>
2022-05-23 17:07:04 +0200

    deployment

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b872663e8bbe0f139b5aa08e57900658'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '803575a5-b173-49dd-8b4a-960d9a4272b6'
+};
